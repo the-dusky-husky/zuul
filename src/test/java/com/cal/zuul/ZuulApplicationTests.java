871cf63d52591153aedfc5af8aeb9927b326c655
Abhisheks <abhisheks@aclmobile.com>
2020-08-25 21:17:21 +0530

    first commit

---
+++ src/test/java/com/cal/zuul/ZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cal.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
